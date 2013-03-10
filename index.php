8150816bc4774cc2f44883d7968063d230a5d934
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2013-03-10 11:20:35 -0400

    inicializacion del proyecto

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC
+    "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>scesi cappuchino</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="content-language" content="es" />
+        <meta http-equiv="title" content="scesi cappuchino" />
+        <meta name="author" content="proyecto mARTadero" />
+        <meta name="distribution" content="global" />
+        <meta name="description" content="gestor de horarios en la fcyt" />
+        <meta name="keywords" content="scesi,pdf,horarios,fcyt,umss" />
+        <meta name="locality" content="Cochabamba, Bolivia" />
+        <meta name="organization" content="SCESI | Sociedad científica de estudiantes de sistemas e informática" />
+        <meta name="origen" content="SCESI | Sociedad científica de estudiantes de sistemas e informática" />
+        <meta name="revisit" content="1 days" />
+
+        <meta name="title" content="scesi cappuchino" />
+        <link href="/media/cup.png" rel="icon" type="image/png" />
+        <link href="/media/css/style.css" media="screen" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div id="header">
+            <ul>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+        <div id="wrapper">
+            <div id="content"></div>
+            <div id="footer"></div>
+        </div>
+    </body>
+</html>
