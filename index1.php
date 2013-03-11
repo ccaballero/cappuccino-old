8385575670cf9ca6df08c6a3a28625a4f5cc3ee7
Carlos Caballero <cijkb.j@gmail.com>
2013-03-11 14:12:59 -0400

    agregado el archivo .htaccess

---
+++ index1.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC
+    "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>scesi cappuchino</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="content-language" content="es" />
+        <meta http-equiv="title" content="scesi cappuchino" />
+        <meta name="author" content="proyecto mARTadero" />
+        <meta name="distribution" content="global" />
+        <meta name="description" content="gestor de horarios en la fcyt" />
+        <meta name="keywords" content="scesi,pdf,horarios,fcyt,umss" />
+        <meta name="locality" content="Cochabamba, Bolivia" />
+        <meta name="organization" content="SCESI | Sociedad científica de estudiantes de sistemas e informática" />
+        <meta name="origen" content="SCESI | Sociedad científica de estudiantes de sistemas e informática" />
+        <meta name="revisit" content="1 days" />
+
+        <meta name="title" content="scesi cappuchino" />
+        <link href="/media/cup.png" rel="icon" type="image/png" />
+        <link href="/media/css/style.css" media="screen" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div id="header">
+            <ul>
+                <li class="active"><a href="index.php">Inicio</a></li>
+                <li><a href="index2.php">Carreras</a></li>
+                <li><a href="index3.php">Materias</a></li>
+                <li><a href="index4.php">Grupos</a></li>
+                <li><a href="index5.php">Horarios</a></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+        <div id="wrapper">
+            <div id="content">
+                <h1>Scesi Cappuchino</h1>
+                <p>En este sitio tu puedes seleccionar tus horarios para el
+                semestre, para este proceso se requiere que usted vaya marcando
+                aquellos elementos que requiere.</p>
+                <p>El proceso consta de varios pasos:</p>
+
+                <div class="wizard">
+                    <div class="step">
+                        <h2>Paso 1: Selección de la carrera</h2>
+                        <p>Se seleccionan las carreras que usted quiera.</p>
+                    </div>
+                    <div class="step">
+                        <h2>Paso 2: Selección de las materias</h2>
+                        <p>Elige las materias que a usted le interesan.</p>
+                    </div>
+                    <div class="step">
+                        <h2>Paso 3: Selección de los grupos</h2>
+                        <p>Marca sus preferencias en cuestion de grupos.</p>
+                    </div>
+                    <div class="step">
+                        <h2>Paso 4: Personalización del horario</h2>
+                        <p>Marca las posibilidades segun sus preferencias.</p>
+                    </div>
+                </div>
+                <div class="clear"></div>
+
+                <p>Para comenzar el proceso correspondiente a la gestión
+                XX/XXXX, usted debe presionar en el boton comenzar:</p>
+
+                <div class="start">
+                    <ul>
+                        <li>
+                            <a href="#">&nbsp;Comenzar&nbsp;&raquo;</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+            <div id="footer">
+                <ul>
+                    <li><a href="http://www.scesi.org/">SCESI</a></li>
+                    <li><a href="http://www.memi.umss.edu.bo/">MEMI</a></li>
+                    <li><a href="https://github.com/ccaballero/cappuchino">Código fuente</a></li>
+                </ul>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </body>
+</html>
