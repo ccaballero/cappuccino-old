c3c3be3a18376a52337cab5c5dfa5a9361eb9de3
Carlos Caballero <cijkb.j@gmail.com>
2013-03-12 13:18:16 -0400

    centralizacion de las peticiones a un controlador principal

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
 
+$valid_pages = array(
+    '',
+    'carreras',
+    'materias',
+    'grupos',
+    'horarios',
+);
+
+$_page = $_GET['page'];
+
+foreach ($valid_pages as $count => $page) {
+    if ($page === $_page) {
+        $_count = $count + 1;
+        include "index$_count.php";
+        exit;
+    }
+}
+
+echo 'pagina invalida';
+
+
+
+
+
