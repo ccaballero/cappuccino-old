99cedc6dd1a6e548f19c026bf2e2e82676bb4133
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2013-08-14 14:54:06 -0400

    [CSS] Corrections

--- public/media/javascript/cappuchino.js
+++ public/media/javascript/cappuchino.js
@@ -1,6 +1,20 @@
-var url='/horarios/'
-var gestion='2-2013'
-var url_gestion=url+gestion
+var Config=new(function(){
+    this.url='/horarios/'
+    this.gestion='2-2013'
+    this.url_gestion=this.url+this.gestion
+
+    this.current_color=0
+    this.carreras=[]
+
+    this.tablero=new Array(6)
+    for(i=0;i<this.tablero.length;i++){
+        var periodos=new Array(20)
+        for(j=0;j<periodos.length;j++){
+            periodos[j]=''
+        }
+        this.tablero[i]=periodos
+    }
+})()
 
 var Templates=new(function(){
     this.carrera='<li name="carrera-{0}"><a class="carrera">{2} ({1})</a></li>'
@@ -9,19 +23,15 @@ var Templates=new(function(){
     this.grupo='<li name="grupo-{0}-{1}-{2}-{3}"><input type="checkbox" /><a class="grupo">Grupo #{4}</a><span>{5}</span></li>'
 })()
 
-var Collections=new(function(){
-    this.carreras=[]
-})()
-
 var Events=new(function(){
     this.clickCarrera=function(){
         var ul=$(this).parent().has('ul')
         if(ul.length===0){
             var i=$(this).parent().attr('name').substring(8)
-            carrera=Collections.carreras[i]
-            $.getJSON(url_gestion+'/'+carrera.codigo+'.json',function(json){
+            carrera=Config.carreras[i]
+            $.getJSON(Config.url_gestion+'/'+carrera.codigo+'.json',function(json){
                 if(typeof carrera.niveles==='undefined'){
-                    Collections.carreras[i]=json
+                    Config.carreras[i]=json
                     Render.renderNiveles(i)
                 }
             })
@@ -57,8 +67,10 @@ var Events=new(function(){
         var i=$(this).parent().attr('name').substring(6).split('-')
         if($(this).is(':checked')){
             Render.renderHorarios(i)
+            $(this).next('a.grupo').addClass('selected')
         }else{
             Render.eliminarHorarios(i)
+            $(this).next('a.grupo').removeClass('selected')
         }
     }
 })()
@@ -66,18 +78,18 @@ var Events=new(function(){
 var Render=new(function(){
     this.renderCarreras=function(){
         $('#options').append('<ul></ul>')
-        for(var i in Collections.carreras){
+        for(var i in Config.carreras){
             $('#options>ul').append(
                 Templates.carrera.format(
                     i,
-                    Collections.carreras[i].codigo,
-                    Collections.carreras[i].nombre))
+                    Config.carreras[i].codigo,
+                    Config.carreras[i].nombre))
         }
         $('a.carrera').click(Events.clickCarrera)
     }
     this.renderNiveles=function(index){
         var li=$('li[name="carrera-'+index+'"]')
-        carrera=Collections.carreras[index]
+        carrera=Config.carreras[index]
         li.append('<ul></ul>')
         for(var i in carrera.niveles){
             li.children('ul').append(
@@ -87,7 +99,7 @@ var Render=new(function(){
     }
     this.renderMaterias=function(index){
         var li=$('li[name="nivel-'+index[0]+'-'+index[1]+'"]')
-        nivel=Collections.carreras[index[0]].niveles[index[1]]
+        nivel=Config.carreras[index[0]].niveles[index[1]]
         li.append('<ul></ul>')
         for(var i in nivel.materias){
             li.children('ul').append(
@@ -97,7 +109,7 @@ var Render=new(function(){
     }
     this.renderGrupos=function(index){
         var li=$('li[name="materia-'+index[0]+'-'+index[1]+'-'+index[2]+'"]')
-        materia=Collections.carreras[index[0]].niveles[index[1]].materias[index[2]]
+        materia=Config.carreras[index[0]].niveles[index[1]].materias[index[2]]
         li.append('<ul></ul>')
         for(var i in materia.grupos){
             li.children('ul').append(
@@ -108,29 +120,31 @@ var Render=new(function(){
     }
     this.renderHorarios=function(index){
         var li=$('li[name="grupo-'+index[0]+'-'+index[1]+'-'+index[2]+'-'+index[3]+'"]')
-        materia=Collections.carreras[index[0]].niveles[index[1]].materias[index[2]]
+        materia=Config.carreras[index[0]].niveles[index[1]].materias[index[2]]
         grupo=materia.grupos[index[3]]
         for(var i in grupo.horarios){
             Render.renderHorario(
                 grupo.horarios[i].dia,
                 grupo.horarios[i].hora,
                 grupo.horarios[i].duracion,
-                materia.nombre+' ('+grupo.horarios[i].aula+')'
+                materia.nombre+' ('+grupo.horarios[i].aula+')',
+                'color'+(Config.current_color+1)
             )
         }
+        Config.current_color=(Config.current_color+1)%9;
     }
-    this.renderHorario=function(dia,hora,duracion,texto){
+    this.renderHorario=function(dia,hora,duracion,texto,color){
         var dias={'LU':3,'MA':4,'MI':5,'JU':6,'VI':7,'SA':8}
         var periodos={'645':2,'730':3,'815':4,'900':5,'945':6,'1030':7,'1115':8,'1200':9,'1245':10,'1330':11,'1415':12,'1500':13,'1545':14,'1630':15,'1715':16,'1800':17,'1845':18,'1930':19,'2015':20,'2100':21}
-        for (var i = 0; i< duracion;i++) { 
+        for (var i = 0; i< duracion;i++) {
             $('#schedule table tbody tr:nth-child('+(periodos[hora]+i)+') td:nth-child('+dias[dia]+')')
                 .text(texto)
-                .addClass('green')
+                .addClass(color)
         }
     }
     this.eliminarHorarios=function(index) {
         var li=$('li[name="grupo-'+index[0]+'-'+index[1]+'-'+index[2]+'-'+index[3]+'"]')
-        materia=Collections.carreras[index[0]].niveles[index[1]].materias[index[2]]
+        materia=Config.carreras[index[0]].niveles[index[1]].materias[index[2]]
         grupo=materia.grupos[index[3]]
         for(var i in grupo.horarios){
             Render.eliminarHorario(
@@ -139,15 +153,15 @@ var Render=new(function(){
                 grupo.horarios[i].duracion
             )
         }
-        
+
     }
     this.eliminarHorario=function(dia,hora,duracion) {
         var dias={'LU':3,'MA':4,'MI':5,'JU':6,'VI':7,'SA':8}
         var periodos={'645':2,'730':3,'815':4,'900':5,'945':6,'1030':7,'1115':8,'1200':9,'1245':10,'1330':11,'1415':12,'1500':13,'1545':14,'1630':15,'1715':16,'1800':17,'1845':18,'1930':19,'2015':20,'2100':21}
-        for (var i = 0; i< duracion;i++) { 
+        for (var i = 0; i< duracion;i++) {
             $('#schedule table tbody tr:nth-child('+(periodos[hora]+i)+') td:nth-child('+dias[dia]+')')
                 .text(' ')
-                .removeClass('green')
+                .removeClass()
         }
     }
 })()
@@ -155,10 +169,10 @@ var Render=new(function(){
 $(document).ready(function(){
     jQuery.fn.exists=function(){return this.length>0;}
 
-    $.getJSON(url_gestion+'.json',function(json){
-        Collections.carreras=json
+    $.getJSON(Config.url_gestion+'.json',function(json){
+        Config.carreras=json
         Render.renderCarreras()
     })
 
-    $('header h1').append(' :: gestión: '+gestion)
+    $('header h1').append(' :: gestión: '+Config.gestion)
 })
