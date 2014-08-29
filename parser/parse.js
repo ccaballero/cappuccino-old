0d2745d7da0fad14c6ce0e470a6c5ff72e7801f2
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-08-29 10:37:50 -0400

    working on parse pdf

--- parser/parse.js
+++ parser/parse.js
@@ -14,8 +14,13 @@ pdftext(path,function(err,chunks){
       , regex2=/^ ?([ABCDEFGHIJ]) ?$/
       , regex3=/^([0-9]{7}) ?([A-Z ]+)/
       , regex4=/^([0-9]{1,2}[A-Z]?)$/
+      , regex5=/^(LU|MA|MI|JU|VI|SA) ([0-9]{3,4})-([0-9]{3,4})\((.*)\)$/
+      , regex6=/^([A-Z .]{4,})$/
+      , regex7=/^\(*\)$/
 
     var flag1=false
+      , flag2=false
+      , flag3=false
 
     var level=undefined
       , subject=undefined
@@ -23,8 +28,15 @@ pdftext(path,function(err,chunks){
     var i1=-1
       , i2=-1
       , i3=-1
+      , i4=-1
 
     var result=undefined
+      , parseDuration=function(s,f){
+            var _s=(parseInt(parseInt(s)/100)*60)+(parseInt(s)%100)
+              , _f=(parseInt(parseInt(f)/100)*60)+(parseInt(f)%100)
+
+            return parseInt((_f-_s)/45)
+        }
 
     chunks.forEach(function(element){
         var match1=regex1.exec(element)
@@ -71,10 +83,52 @@ pdftext(path,function(err,chunks){
             if(!group||group!=match4[1]){
                 result.levels[i1].subjects[i2].groups.push({
                     code:match4[1]
+                  , schedule:[]
                 });
                 group=match4[1];
                 i3++;
+                i4=-1;
             }
+        }
+
+        var match5=regex5.exec(element)
+        if(match5){
+            result.levels[i1].subjects[i2].groups[i3].schedule.push({
+                day:match5[1]
+              , start:match5[2]
+              , end:match5[3]
+              , duration:parseDuration(match5[2],match5[3])
+              , room:match5[4]
+            });
+            i4++;
+            flag2=true;
+        }
+
+        var match6=regex6.exec(element)
+        if(match6&&flag2){
+            var groups=result.levels[i1].subjects[i2].groups[i3]
+
+            if(!groups.teacher){
+                result.levels[i1].subjects[i2]
+                      .groups[i3].teacher=match6[1];
+            }else{
+                var teacher=groups.teacher
+
+                if(teacher!=match6[1]){
+                    result.levels[i1].subjects[i2]
+                          .groups[i3].teacher=[teacher,match6[1]];
+                }
+            }
+
+            result.levels[i1].subjects[i2]
+                  .groups[i3].schedule[i4].teacher=match6[1];
+            flag2=false;
+            flag3=true;
+        }
+
+        var match7=regex7.exec(element)
+        if(match7&&flag3){
+            flag3=false;
         }
     });
 
