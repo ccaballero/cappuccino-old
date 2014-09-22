6b47cd53f24cbac1e8a47520c738359e9cb949d4
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-09-22 04:04:08 -0400

    working on ember controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,24 @@ App.Router.map(function(){
     this.route('index',{
         path:'/'
     });
+    this.route('schedule',{
+        path:'/:f/:g'
+    });
 });
 
 App.IndexRoute=Ember.Route.extend({
     model:function(){
         return Ember.$.getJSON('/data/summary.json');
+    }
+});
+
+App.ScheduleRoute=Ember.Route.extend({
+    model:function(params){
+        // TODO need validators
+        var f=params.f
+          , g=params.g;
+        
+        return Ember.$.getJSON('/data/'+f+'/'+g+'.json');
     }
 });
 
