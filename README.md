6c079c79fef1593d3e6f04d1dd26922fdd33f4e7
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-09-28 18:54:30 -0400

    workin on offcanvas

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-Cappuccino
-==========
+SCESI Cappuccino
+======______====
 
 Sistema para la visualización decente de horarios para las carreras en la 
-facultad de ciencias y tecnologia, esta nueva version totalmente repensada para
-fullstack javascript, ahora si veremos a como nos toca.... XD
+facultad de ciencias y tecnologia.
+
+Esta es la nueva version totalmente repensada para correr con lo ultimo de las
+herramientas JavaScript...
+
 
 
 
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,6 @@ Ember.Handlebars.helper('img',function(str){
         '<img src="/images/'+str.toLowerCase()+'.svg" />');
 });
 Ember.Handlebars.helper('s_career',function(str){
-    console.log(str);
     return new Ember.Handlebars.SafeString(
         '<a href="">('+str.code+')'+
         str.name.toUpperCase().slice(15,17)+
@@ -16,26 +15,36 @@ Ember.Handlebars.helper('s_career',function(str){
 App.Store=DS.Store.extend();
 
 App.Router.map(function(){
-    this.route('index',{
-        path:'/'
-    });
     this.route('schedule',{
         path:'/:f/:g'
     });
 });
 
+App.ApplicationController=Ember.ObjectController.extend({
+    context: ''
+});
+
 App.IndexRoute=Ember.Route.extend({
     model:function(){
+        // TODO convert in store
         return Ember.$.getJSON('/data/summary.json');
     }
 });
 
 App.ScheduleRoute=Ember.Route.extend({
+    setupController:function(controller,model){
+        this.controllerFor('application').set('context',this.get('f'));
+        controller.set('model',model);
+    },
     model:function(params){
+        // TODO convert in store
         // TODO need validators
         var f=params.f
           , g=params.g;
         
+        this.set('f',f);
+        this.set('g',g);
+
         return Ember.$.getJSON('/data/'+f+'/'+g+'.json');
     }
 });
